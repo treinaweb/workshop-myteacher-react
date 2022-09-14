f586fda1d15b5a627e9bb59643a8a6ebb26cd724
Dev7 <dev7@coolsis.pt>
2022-09-14 10:41:16 -0300

    04 - Olá mundo e pagina de exemplo

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Olá Mundo Treinaweb!
         </h1>
 
         <p className={styles.description}>
