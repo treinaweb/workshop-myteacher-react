83e5b37a05f74bb05745fcf826c20c35afc624ee
Dev7 <dev7@coolsis.pt>
2022-09-14 13:16:31 -0300

    06 - Entendendo o JSX

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,9 @@
 import type { NextPage } from 'next'
 
-function Botao(props){
-  return <a href="#">{props.titulo}</a>;
-}
-
 const Home: NextPage = () => {
   return (
    <div>
-    <Botao titulo="Click" /><br />
-    <Botao titulo="Entrar"  /><br />
-    <Botao titulo="Ver"  /><br />
-    <Botao titulo="Ver Mais"  />
+    treinaweb
    </div>
   )
 }
