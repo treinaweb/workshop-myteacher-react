6c6d685d14e0b73847c280187898bddfc6bd19db
Dev7 <dev7@coolsis.pt>
2022-09-15 10:44:22 -0300

    18 - prop key e formatar valor

--- src/components/Lista/Lista.tsx
+++ src/components/Lista/Lista.tsx
@@ -12,11 +12,11 @@ const Lista = (props: ListaProps) => {
             {props.professores.length > 0 ? (
                 <ListaStyled>
                     {props.professores.map(professor => (
-                        <ItemLista>
+                        <ItemLista key={professor.id}>
                             <Foto src={professor.foto}></Foto>
                             <Informacoes>
                                 <Nome>{professor.nome}</Nome>
-                                <Valor>{professor.valor_hora} por hora</Valor>
+                                <Valor>{professor.valor_hora.toLocaleString('pt-BR', { minimumFractionDigits: 2, style: 'currency', currency: 'BRL' })} por hora</Valor>
                                 <Descricao>{professor.descricao}</Descricao>
                                 <Button sx={{ width: '70%' }}>Marcar Aula com Elton</Button>
                             </Informacoes>
